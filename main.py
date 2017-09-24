a5ab34179ea41eaa0862eab7314f2ddd1dd68255
Shonda Cogshell <scogshell2001@yahoo.com>
2017-09-24 15:06:27 -0500

    finished

--- main.py
+++ main.py
@@ -4,50 +4,52 @@ from caesar import rotate_string
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form="""<!DOCTYPE html>
+form = """<!DOCTYPE html>
+
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
-        <body>
-            <form action= "/rotate" method="POST">
-
-                <label for= "rot">
-                    Rotate by:
-                    <input type="text" name="rot" value=0 /> <br>
-                </label> <br> 
-                    <input type="textarea" name="text" /> <br>
-                    <input type="submit" value ="Submit Query"/>
-            </form>
-        </body>
-</html>"""
+    <body>
+      <form action="/encrypt" method = "POST">
+        <label for="rot">Rotate by: </label>
+        <input type = "text" name ="rot" value="0"></input>
+        <textarea name = "text">{0}</textarea>
+        <input type="submit"></input>
+      </form>
+    </body>
+</html>
+        """
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 
 
-@app.route("/rotate", methods=['POST'])
+@app.route("/encrypt", methods=['POST'])
 def encrypt():
-    rot= int(request.form['rot'])
-    text= request.form['text']
-    encypted_string= rotate_string(text,rot)
-    return encypted_string
+    text = request.form["text"]
+    rot= request.form["rot"]
+    rot = int(rot)
+    encryped_text = rotate_string(text,rot)
+
+    return form.format(encryped_text)
+    
     
 
 
